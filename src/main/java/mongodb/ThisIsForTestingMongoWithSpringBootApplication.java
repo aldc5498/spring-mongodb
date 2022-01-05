8437d35d17a856704c267b4ec009526e8a06c9a7
alish <aldc1233@gmail.com>
2022-01-05 19:42:21 +0545

    first commit

---
+++ src/main/java/mongodb/ThisIsForTestingMongoWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisIsForTestingMongoWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisIsForTestingMongoWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mongodb/ThisIsForTestingMongoWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisIsForTestingMongoWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
